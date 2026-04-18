330b209e05dbecd454a0c426a3ea4ae82bbcce02
Tilek Kubanychbekov <tkubanyc@2-D-3.42heilbronn.de>
2026-04-18 20:31:06 +0200

    Add 1 global parser to handle both cases for strict and free form entry modes

---
+++ parser.py
@@ -0,0 +1,17 @@
+from parser_strict import parse_equation_strict
+from parser_free import parse_equation_free
+
+Term = tuple[float, int]
+
+
+def parse_equation(equation: str, mode: str = "strict") -> tuple[list[Term], list[Term]]:
+    """
+    Dispatcher parser.
+    - strict: mandatory format (a*X^p)
+    - free: bonus free-form input
+    """
+    if mode == "strict":
+        return parse_equation_strict(equation)
+    if mode == "free":
+        return parse_equation_free(equation)
+    raise ValueError(f"Unknown parser mode: '{mode}'")
